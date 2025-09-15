79afa9bacf0d3418adb171fed55816c771fa4969
LancD <Vorngviseth077@gmail.com>
2025-09-16 00:53:25 +0700

    idk man

--- Godotproject/scripts/goblin.gd
+++ Godotproject/scripts/goblin.gd
@@ -5,6 +5,7 @@ const SPEED = 100.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var attacking = false
+var chasing = false
 
 func _physics_process(delta: float) -> void:
 	if attacking:
@@ -12,27 +13,38 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 		return
 	
-	if target:
+	if chasing and target:
 		var direction = (target.position - position).normalized()
+		
 		velocity = direction * SPEED
-		
 		animated_sprite.play("walk")
-		
+				
 		if direction.x > 0:
 			animated_sprite.flip_h = false
 		elif direction.x < 0:
 			animated_sprite.flip_h = true
-	
+	else:
+		velocity = Vector2.ZERO
+		animated_sprite.play("idle")
+		
 	move_and_slide()
 
+#Detection zone 
+func _on_detection_zone_body_entered(body: Node2D) -> void:
+	if body.name == "guard":
+		chasing = true
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_detection_zone_body_exited(body: Node2D) -> void:
+	if body.name == "guard":
+		chasing = false
+
+#Attack Zone
+func _on_attack_zone_body_entered(body: Node2D) -> void:
 	if body.name == "guard":
 		attacking = true
 		velocity = Vector2.ZERO
 		animated_sprite.play("attack")
 
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
-		if body.name == "guard":
-			attacking = false
+func _on_attack_zone_body_exited(body: Node2D) -> void:
+	if body.name == "guard":
+		attacking = false
